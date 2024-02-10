c03a73a756249951fc494f5e2a2559ab52aaef7d
alina-alieks <145293161+alina-alieks@users.noreply.github.com>
2024-02-10 16:02:04 +0200

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,6 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const images = [
   {
     preview:
@@ -81,8 +84,7 @@ const galleryHTML = images.map(el => {
 
 gallery.insertAdjacentHTML("afterbegin", galleryHTML);
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+
 const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
 
 lightbox.on('shown.simplelightbox', function () {
